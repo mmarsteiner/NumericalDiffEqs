f040bfd3a076116ff5f80d37248466c378d76042
Matthew <mmarsteiner@gmail.com>
2024-01-27 13:29:34 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,6 @@ import java.util.Scanner;
 import java.util.function.BiFunction;
 
 public class Main {
-    public enum MODE {
-        EULER,IMP_EULER,RK4
-    }
-
     public static void main(String[] args) {
         JFrame frame = new JFrame("Numerical Differential Equation Solver 1.0");
         NumericalSolverGUI gui = new NumericalSolverGUI();
@@ -21,13 +17,5 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-    }
-
-    static NumericalSolver supplySolver(MODE mode) {
-        return switch (mode) {
-            case EULER -> new EulerSolver();
-            case IMP_EULER -> new ImpEulerSolver();
-            case RK4 -> new RK4Solver();
-        };
     }
 }
